301d2fcc03bed50fdd9e968b5316e645451cdb5a
Farik2605 <farik63@gmail.com>
2016-01-08 13:11:55 +0300

    redmine ref. 6

--- app/code/community/Kontenta/CodesWholesale/sql/kontenta_cw_attribute_setup/mysql4-install-0.1.0.php
+++ app/code/community/Kontenta/CodesWholesale/sql/kontenta_cw_attribute_setup/mysql4-install-0.1.0.php
@@ -6,7 +6,7 @@ $installer->startSetup();
 $installer->addAttribute('catalog_product','kontenta_corresponding_product', array (
     'group' => 'General',
     'label'    => 'Corresponding Product',
-    'visible'     => true,
+    'visible'     => false,
     'type'     => 'varchar',
     'input'    => 'text',
     'system'   => false,
